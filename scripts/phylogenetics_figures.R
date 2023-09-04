963facae063512e500b1431a09a9f74b53190a76
savvas-paragkamian <s.paragkamian@gmail.com>
2023-09-04 14:24:45 +0300

    analysis with R

---
+++ scripts/phylogenetics_figures.R
@@ -0,0 +1,19 @@
+#!/usr/bin/env Rscript
+
+###############################################################################
+# script name: phygenetics_figures.R
+# developed by: Savvas Paragkamian
+# framework: SarrisLab
+###############################################################################
+# GOAL:
+# Aim of this script is to analyse and  visualise the de novo trees from the
+# gtdb-tk pipeline.
+###############################################################################
+# usage:./phygenetics_figures.R
+###############################################################################
+
+library(tidyverse)
+library(ape)
+library(ggtree)
+
+
