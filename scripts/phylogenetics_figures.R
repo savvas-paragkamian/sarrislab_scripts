975630823d01e8efe140da10eaf3736cefa6bf1e
savvas-paragkamian <s.paragkamian@gmail.com>
2023-09-04 18:22:09 +0300

    figure of the family ready for brainstorming

--- scripts/phylogenetics_figures.R
+++ scripts/phylogenetics_figures.R
@@ -27,9 +27,30 @@ taxonomy_w <- taxonomy %>% group_by(gtdb_id, classification) %>%
               .groups = "drop") %>%  
     pivot_wider(names_from=classification, values_from=taxa)
 
+# load tree
 tree_543 <- ape::read.tree(file = "../543_de_novo/infer/gtdbtk.bac120.decorated.tree")
+tree_543_taxonomy <- as_tibble(tree_543) %>% left_join(taxonomy_w, by=c("label"="gtdb_id"))
 
+# the node 82141 is the family
+claude_f__DSM_18226 <- groupClade(as_tibble(tree_543), 82141) %>%
+    filter(group==1) %>% 
+    left_join(taxonomy_w,
+              by=c("label"="gtdb_id")) %>% 
+    as.treedata()
 
-claude_f__DSM_18226 <- groupClade(as_tibble(tree_543), 82141)
 tree_543_long <- as_tibble(tree_543)
 as.treedata(tree_543_long)
+
+# visualisation
+
+tree_plot <- ggtree(data = claude_f__DSM_18226,
+               aes(color=g)) + 
+     geom_tiplab(size=2, aes(label= s, color=g)) + 
+     theme_tree(legend.posision = "bottom")
+    
+ggsave(plot=tree_plot,
+       "../tree_plot1.png",
+       device="png",
+       height = 30,
+       width=30,
+       units="cm")
