ead2046db45200e8c702dbf0c245a4d328289233
savvas-paragkamian <s.paragkamian@gmail.com>
2023-09-04 17:20:07 +0300

    tree and taxonomy loaded

--- scripts/phylogenetics_figures.R
+++ scripts/phylogenetics_figures.R
@@ -15,5 +15,21 @@
 library(tidyverse)
 library(ape)
 library(ggtree)
+library(tidytree)
+
+# read the taxonomy
+taxonomy <- read_delim( "../543_de_novo/infer/gtdbtk.taxonomy_long.tsv",
+                       delim="\t",
+                       col_names=T)
+# transform taxonomy to wide format
+taxonomy_w <- taxonomy %>% group_by(gtdb_id, classification) %>%
+    summarise(taxa=paste(taxonname, collapse = "; "),
+              .groups = "drop") %>%  
+    pivot_wider(names_from=classification, values_from=taxa)
+
+tree_543 <- ape::read.tree(file = "../543_de_novo/infer/gtdbtk.bac120.decorated.tree")
 
 
+claude_f__DSM_18226 <- groupClade(as_tibble(tree_543), 82141)
+tree_543_long <- as_tibble(tree_543)
+as.treedata(tree_543_long)
